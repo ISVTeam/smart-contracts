0b79ad7d28db92d483ab013adc3d5d20d569f01f
vladimir <vova.sarckisyan@yandex.ru>
2017-08-18 17:30:27 +0300

    Fix convert wei

--- own-currency.sol
+++ own-currency.sol
@@ -1,64 +1,75 @@
 /*
-Powered by:
-
-Kuznetsoff.Laboratory
-http://kuznecov-lab.ru
-info@kuznecov-lab.ru
-
+Powered by:Kuznetsoff.Laboratory
+http://7905415.ru
+andrey@kuznetsoff.com
 ISVTeam
-https://isvteam.com/
 info@isvteam.com
+For Arm-Money, INC
+http://ArmMoneyINC.com
+armmoneyru@gmail.com
 @version 1.0.1
 */
 pragma solidity ^0.4.11;
 contract ERC20 {
-    uint public ex;
-    address public owner;
     uint public totalSupply;
     function balanceOf(address who) constant returns (uint);
     function transfer(address to, uint value);
     event Transfer(address indexed from, address indexed to, uint value);
 }
-contract YKN_07 is ERC20 {
+contract AMT is ERC20 {
     string public constant name = "Arm-Money Token (AMT)";
     string public constant symbol = "AMT";
-    uint8 public constant decimals = 18;
+    uint8 public constant decimals = 0;
     mapping (address => uint) balances;
-
-    function YKN_07(uint _ex, uint initialCount) {
+    // Custom properties
+    uint public ex;
+    address public owner;
+    // Modifiers
+    modifier ownerOnly() {
+        if (msg.sender != owner) {
+            revert();
+        }_;
+    }
+    // Constructor - will be call only on contract creation
+    function AMT(uint _ex, uint initialCount) {
         balances[msg.sender] = initialCount;
         owner = msg.sender;
-        ex = _ex;
         totalSupply = initialCount;
+        setExchangePrice(_ex);
     }
 
+    // The main contract function
     function buyTokens() payable external {
         if (msg.value == 0) revert();
-        var numTokens = msg.value*ex; // Convert wei to Ethereum
-        if(balances[owner] < numTokens) revert(); // Check if owner have tokens
+        var numTokens = msg.value/ex; // Calculate count of tokens
+        if(numTokens == 0) revert();
         balances[msg.sender] += numTokens; // Update buyer balance
         balances[owner]  -= numTokens; // Update seller balance
         totalSupply = balances[owner]; // Update total tokens count
         Transfer(owner, msg.sender, numTokens); // Call Transfer event
     }
 
+    // Transfer tokens between members using this contract functionality
     function transfer(address _to, uint _value) {
+        if(balanceOf(msg.sender) < _value) revert(); // Check if sender have tokens
+        // Update balances
         balances[msg.sender] -= _value;
         balances[_to] += _value;
         Transfer(msg.sender, _to, _value);
     }
 
+    // Get tokens count by address
     function balanceOf(address _owner) constant returns (uint balance) {
         return balances[_owner];
     }
 
-    function setExchangePrice(uint _ex)
-    {
-        if(msg.sender != owner) revert();
+    // Set sell price per one token
+    function setExchangePrice (uint _ex) ownerOnly {
         ex = _ex;
     }
 
-    function kill() {
+    // Destruct contract and send funds to contract owner
+    function kill() ownerOnly {
         if(msg.sender == owner) selfdestruct(owner);
     }
 }
